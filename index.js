cbfe58b93d1fe75a60a184913f9a636c0b5ae3c4
TheTomozak <tomaszpalczewski99@gmail.com>
2020-08-11 15:11:48 +0200

    Dodano moduły Node'a

---
+++ index.js
@@ -0,0 +1,3 @@
+
+const userInfo = require('./user');
+console.log(userInfo);
---
+++ user.js
@@ -0,0 +1,6 @@
+const user = {
+    avatar_url: 'https://picsum.photos/300',
+    bio: 'Test bio'
+};
+
+module.exports= user;
