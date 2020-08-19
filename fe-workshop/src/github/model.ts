b293bbce35333a1c6fe242096d2d95bb35eb14c4
TheTomozak <tomaszpalczewski99@gmail.com>
2020-08-19 13:15:29 +0200

    Dodano metode pobierajaca repozytoria z GithHub'a

--- fe-workshop/src/github/model.ts
+++ fe-workshop/src/github/model.ts
@@ -1,20 +1,42 @@
+export interface IGithubUserResponse {
+    avatar_url?: string;
+    bio?: string;
 
-export interface IGithubResponse{
-  avatar_url?: string;
-  bio?: string;
+}
+
+export interface IGitHubRepoResponse {
+    name: string;
+    stargazers_count: number;
+    fork: boolean;
+    html_url: string;
+
+}
+
+
+export class GitHubRepo {
+    html_url: string;
+    name: string;
+    stargazers_count: number;
+
+    constructor({html_url: url, name, stargazers_count: stars}: IGitHubRepoResponse) {
+        this.html_url = url;
+        this.name = name;
+        this.stargazers_count = stars;
+    }
+
 
 }
 
 
 export class GitHubUser {
-  public img: string;
-  public bio: string;
+    public img: string;
+    public bio: string;
 
-  constructor({ avatar_url: img = '', bio = 'Cannot read a biography' } : IGithubResponse = {}) {
-    [this.img, this.bio] = [img, bio];
-  }
+    constructor({avatar_url: img = '', bio = 'Cannot read a biography'}: IGithubUserResponse = {}) {
+        [this.img, this.bio] = [img, bio];
+    }
 
-  toString() {
-    return this.bio;
-  }
+    toString() {
+        return this.bio;
+    }
 }
--- fe-workshop/src/github/service.ts
+++ fe-workshop/src/github/service.ts
@@ -1,6 +1,6 @@
-import { GitHubUser } from './model';
+import {GitHubRepo, GitHubUser, IGitHubRepoResponse} from './model';
 
-export async function getUser(name: string) {
+export async function getUser(name: string) : Promise<GitHubUser> {
   try {
     const resp = await fetch(`https://api.github.com/users/${name}`);
     return new GitHubUser(await resp.json());
@@ -9,3 +9,18 @@ export async function getUser(name: string) {
     return new GitHubUser();
   }
 }
+
+
+export async function getRepoGitHub(name: string) : Promise<GitHubRepo[]> {
+  try {
+    const resp = await fetch(`https://api.github.com/users/${name}/repos`);
+   return (await resp.json() as IGitHubRepoResponse[])
+        .filter(r => !r.fork)
+        .map(r => new GitHubRepo(r));
+  } catch (e) {
+    console.warn(e);
+    return [];
+  }
+
+}
+
