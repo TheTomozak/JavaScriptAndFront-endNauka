67e713159b50d078ae8d0324bb4e3337ef7dd427
TheTomozak <tomaszpalczewski99@gmail.com>
2020-08-20 15:16:56 +0200

    Dodano react'a

---
+++ fe-workshop/src/index.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+
+import initAboutMe from './about-me/index';
+
+
+initAboutMe();
+
+
+
+ReactDOM.render(
+    <h1>Hello, world!</h1>,
+    document.getElementById('repos')
+);
